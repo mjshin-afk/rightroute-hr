78acc79a687401cf88d3111081ca44777e833597
mjshin-afk <mj.shin@rightroute.kr>
2025-12-24 15:14:13 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "RightRoute HR is running"
+
+@app.get("/health")
+def health():
+    return "ok"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
